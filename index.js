20465a3c9c5839c37959f531aa75a70c32915dfa
Francesco (makevoid) <makevoid@gmail.com>
2015-04-03 03:42:57 +0100

    too fun like this

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
 var bitcore     = require('bitcore')
 var Handlebars  = require('handlebars')
 
-var privateKey  = new bitcore.PrivateKey();
-var address     = privateKey.publicKey.toAddress();
-
-
-console.log("privateKey", privateKey.toString(), privateKey)
-console.log("address",    address.toString(),    address   )
-
-
-
-var source   = $("#address-tpl").html()
-var template = Handlebars.compile(source)
-
-var context  = {
-  address: address,
-  privateKey: privateKey
+// store
+var store  = {
+  keys: []
 }
-var html     = template(context)
+
+// bitcore actions - add keypair to store
+var bitcoreActions = {
+  addKey: function() {
+    store.keys.push(this._generateKey())
+  },
+
+  _generateKey: function() {
+    var privateKey  = new bitcore.PrivateKey()
+    var address     = privateKey.publicKey.toAddress()
+    // console.log("privateKey", privateKey.toString(), privateKey)
+    // console.log("address",    address.toString(),    address   )
+    return { privateKey: privateKey, address: address }
+  }
+}
 
 
-var div = document.createElement("div");
-div.innerHTML = html
-document.querySelector("body").appendChild(div)
+// seed data
+bitcoreActions.addKey()
+bitcoreActions.addKey()
+
+
+
+
+// templating lib
+var templates = {
+  compile: function(tmplName){
+    return Handlebars.compile(
+      $("#"+tmplName+"-tpl").html()
+    )
+  },
+
+  all: {},
+
+  init: function(templates) {
+    templates.forEach(function(tpl){
+      this.all[tpl] = this.compile(tpl)
+    }.bind(this))
+  }
+}
+
+// templating
+var templateNames = ["address"]
+templates.init(templateNames)
+
+
+
+// rendering
+var renderAddress = function(address) {
+  var html = templates.all.address(address)
+  var div = document.createElement("div");
+  div.innerHTML = html
+  document.querySelector("body").appendChild(div)
+}
+
+
+// store render
+store.keys.forEach(function(address){
+  renderAddress(address)
+})
+
+
+// handlers
+
+$("#reveal-pvt-key").on("click", function(evt){
+  console.log(evt.target)
+})
