18c7d26c189dbb297ddf8a123bb8b9cf77d6adc9
Francesco (makevoid) <makevoid@gmail.com>
2015-04-03 03:23:41 +0100

    add zepto and handlebars

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var bitcore = require('bitcore')
+var bitcore     = require('bitcore')
+var Handlebars  = require('handlebars')
 
 var privateKey  = new bitcore.PrivateKey();
 var address     = privateKey.publicKey.toAddress();
@@ -6,3 +7,19 @@ var address     = privateKey.publicKey.toAddress();
 
 console.log("privateKey", privateKey.toString(), privateKey)
 console.log("address",    address.toString(),    address   )
+
+
+
+var source   = $("#address-tpl").html()
+var template = Handlebars.compile(source)
+
+var context  = {
+  address: address,
+  privateKey: privateKey
+}
+var html     = template(context)
+
+
+var div = document.createElement("div");
+div.innerHTML = html
+document.querySelector("body").appendChild(div)
