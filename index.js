db44eb937cbc0109d23bbe433e20132930a28da9
Francesco (makevoid) <makevoid@gmail.com>
2015-04-05 17:40:37 +0100

    prototyping a wallet class

--- index.js
+++ index.js
@@ -1,5 +1,87 @@
 var bitcore     = require('bitcore')
 var Handlebars  = require('handlebars')
+var get         = require("get-next")
+
+var BchainApi = {
+
+  unspent: function(address) {
+    this.getJson this._unspentUrl(address)
+  },
+
+  _unspentUrl: function(address) {
+    return "https://blockchain.info/unspent?active="+address+"&format=json"
+  }
+
+}
+
+
+var bitcoin = {
+
+  init: function() {
+    this._generateKeypair()
+  },
+
+  privateKey: function() {
+    return this.privateKey;
+  },
+
+  address: function() {
+    return this.address;
+  },
+
+  // sends [amount] to each address
+  send: function(amount, addresses) {
+    var transaction = this._buildTransaction(amount, addresses)
+    return this._broadcastTransaction(transaction)
+  },
+
+
+  // private
+
+  _generateKeyPair: function() {
+    this.privateKey = new bitcore.PrivateKey()
+    this.address    = this.privateKey.publicKey.toAddress()
+  },
+
+  // query for unspent outputs
+  _unspentOutputs: function() {
+    return BchainApi.unspent(address);
+  },
+
+  _signTransaction: function() {
+
+  },
+
+  _broadcastTransaction: function(transaction) {
+    transaction // ...
+  },
+
+  _buildTransaction: function(amount, addresses) {
+    // TODO: right now sends [amount] only to the first address
+
+    var minimum_non_dust_amount = 5430 // .fee(minimum_non_dust_amount)
+
+    var address = addresses[0]
+
+    return new bitcore.Transaction()
+      .from({
+        "address": address,
+        "txid":    "695918d85f25c29ecd3d403b02eef398eda136c5136958041c2e18b0d27dce83",
+        "vout":0,
+        "scriptPubKey":"76a91402efd4b4f35379ca41188b624f63449c5d26b7bf88ac",
+        "amount": amount
+      })
+
+      .from(utxos)          // Feed information about what unspent outputs one can use
+    .to(address, amount)  // Add an output with the given amount of satoshis
+    .change(address)      // Sets up a change address where the rest of the funds will go
+    .sign(privkeySet)     // Signs all the inputs it can
+  },
+
+
+
+}
+
 
 // models (defaults)
 var models = {
